b9207eef24858cbe4deb51853cd79bddc550905f
Wesley Westelley <wesleywestelley@gmail.com>
2022-04-05 01:38:20 -0300

    :test_tube: chore: create provider in memory for bcrypt

---
+++ src/tests/providers/In-memory-bcrypt-provider.ts
@@ -0,0 +1,12 @@
+/* eslint-disable prettier/prettier */
+import { IBcryptProvider } from "../../api/providers/IBcryptProvider";
+
+export class InMemoryBcryptProvider implements IBcryptProvider {
+    public async compare(
+        password: string,
+        userPassword: string,
+    ): Promise<boolean> {
+        if (password === userPassword) return true;
+        return false;
+    }
+}
