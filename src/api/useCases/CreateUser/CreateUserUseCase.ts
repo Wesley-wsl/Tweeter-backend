cd0a45cd3a95cb1a798ae37bf8510089a1163a74
Wesley Westelley <wesleywestelley@gmail.com>
2022-04-07 16:16:44 -0300

    :wrench: feat: create use case for user

---
+++ src/api/useCases/CreateUser/CreateUserUseCase.ts
@@ -0,0 +1,28 @@
+import { hash } from "bcrypt";
+
+import { User } from "../../entities/User";
+import { IUsersRepository } from "../../repositories/IUsersRepository";
+import { ICreateUserDTO } from "./CreateUserDTO";
+
+export class CreateUserUseCase {
+    constructor(private usersRepository: IUsersRepository) {}
+
+    async execute({ name, email, password }: ICreateUserDTO): Promise<User> {
+        const user = await this.usersRepository.findByEmail(email);
+
+        if (user) throw new Error("User already exists");
+        if (!password) throw new Error("Password is required");
+
+        const passwordHashed = await hash(password as string, 8);
+        const userCreated = await this.usersRepository.createUser({
+            name,
+            email,
+            password: passwordHashed,
+            
+        });
+
+        await this.usersRepository.save(userCreated);
+        delete userCreated.password;
+        return userCreated;
+    }
+}
