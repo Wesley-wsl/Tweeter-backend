2280c18b9440577eff5de8c24fb8d045b4f42c8c
Wesley Westelley <wesleywestelley@gmail.com>
2022-04-07 16:14:55 -0300

    :wrench: feat: create controller for create user

---
+++ src/api/useCases/CreateUser/CreateUserController.ts
@@ -0,0 +1,22 @@
+import { Request, Response } from "express";
+
+import { CreateUserUseCase } from "./CreateUserUseCase";
+
+export class CreateUserController {
+    constructor(private createUserUseCase: CreateUserUseCase) {}
+
+    async handle(request: Request, response: Response): Promise<Response> {
+        const { name, email, password } = request.body;
+
+        const user = await this.createUserUseCase.execute({
+            name,
+            email,
+            password,
+        });
+
+        return response.status(201).json({
+            success: true,
+            user,
+        });
+    }
+}
