2b44d2a4f6f6f20ed812cbca53686539884fdda4
Wesley Westelley <wesleywestelley@gmail.com>
2022-04-08 01:05:51 -0300

    :test_tube: test: ensure that authenticate user is work

---
+++ src/api/useCases/AuthenticateUser/AuthenticateUserUseCase.spec.ts
@@ -0,0 +1,110 @@
+import { InMemoryBcryptProvider } from "../../../tests/providers/In-memory-bcrypt-provider";
+import { InMemoryUsersRepository } from "../../../tests/repositories/In-memory-users-repository";
+import { User } from "../../entities/User";
+import { AuthenticateUserUseCase } from "./AuthenticateUserUseCase";
+
+describe("#AuthenticateUser", () => {
+    it("Don't must be able to login user without password.", async () => {
+        const inMemoryUsersRepository = new InMemoryUsersRepository();
+        const inMemoryBcryptProvider = new InMemoryBcryptProvider();
+        const sut = new AuthenticateUserUseCase(
+            inMemoryUsersRepository,
+            inMemoryBcryptProvider,
+        );
+
+        const createUser = {
+            email: "jorkis@gmail.com",
+            password: "1234",
+        };
+
+        inMemoryUsersRepository.items.push(createUser as User);
+
+        const user = {
+            email: "jorkis@gmail.com",
+        };
+        const response = sut.execute(user);
+
+        await expect(response).rejects.toEqual(
+            new Error("Password is required"),
+        );
+    });
+
+    it("Don't must be able to login user with email incorrect.", async () => {
+        const inMemoryUsersRepository = new InMemoryUsersRepository();
+        const inMemoryBcryptProvider = new InMemoryBcryptProvider();
+        const sut = new AuthenticateUserUseCase(
+            inMemoryUsersRepository,
+            inMemoryBcryptProvider,
+        );
+
+        const createUser = {
+            email: "jorkis@gmail.com",
+            password: "1234",
+        };
+
+        inMemoryUsersRepository.items.push(createUser as User);
+
+        const user = {
+            email: "anotheremail@gmail.com",
+            password: "1234",
+        };
+        const response = sut.execute(user);
+
+        await expect(response).rejects.toEqual(
+            new Error("Email/password incorrect"),
+        );
+    });
+
+    it("Don't must be able to login user with password incorrect.", async () => {
+        const inMemoryUsersRepository = new InMemoryUsersRepository();
+        const inMemoryBcryptProvider = new InMemoryBcryptProvider();
+        const sut = new AuthenticateUserUseCase(
+            inMemoryUsersRepository,
+            inMemoryBcryptProvider,
+        );
+
+        const createUser = {
+            email: "jorkis@gmail.com",
+            password: "1234",
+        };
+
+        inMemoryUsersRepository.items.push(createUser as User);
+
+        const user = {
+            email: "jorkis@gmail.com",
+            password: "123456",
+        };
+        const response = sut.execute(user);
+
+        await expect(response).rejects.toEqual(
+            new Error("Email/password incorrect"),
+        );
+    });
+
+    it("Should be able login user with email and password.", async () => {
+        const inMemoryUsersRepository = new InMemoryUsersRepository();
+        const inMemoryBcryptProvider = new InMemoryBcryptProvider();
+        const sut = new AuthenticateUserUseCase(
+            inMemoryUsersRepository,
+            inMemoryBcryptProvider,
+        );
+
+        const createUser = {
+            email: "jorkis@gmail.com",
+            password: "1234",
+        };
+
+        inMemoryUsersRepository.items.push(createUser as User);
+
+        const user = {
+            email: "jorkis@gmail.com",
+            password: "1234",
+        };
+        const response = await sut.execute(user);
+
+        expect(response.user.email).toEqual(createUser.email);
+        expect(response.user.password).toEqual(createUser.password);
+        expect(response.token).toBeTruthy();
+        expect(response.user).toBeTruthy();
+    });
+});
