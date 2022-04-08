971bd585cb5bfe4ed01d1101178d9012413b2210
Wesley Westelley <wesleywestelley@gmail.com>
2022-04-08 01:06:11 -0300

    :white_check_mark: feat: create use case for authenticate user

---
+++ src/api/useCases/AuthenticateUser/AuthenticateUserUseCase.ts
@@ -0,0 +1,47 @@
+import { sign } from "jsonwebtoken";
+
+import { User } from "../../entities/User";
+import { IBcryptProvider } from "../../providers/IBcryptProvider";
+import { IUsersRepository } from "../../repositories/IUsersRepository";
+import { IAuthenticateUserDTO } from "./AuthenticateUserDTO";
+
+export class AuthenticateUserUseCase {
+    constructor(
+        private usersRepository: IUsersRepository,
+        private bcryptProvider: IBcryptProvider,
+    ) {}
+
+    async execute({
+        email,
+        password,
+    }: IAuthenticateUserDTO): Promise<{ token: string; user: User }> {
+        const user = await this.usersRepository.findByEmail(email);
+
+        if (!user) throw new Error("Email/password incorrect");
+        if (!password) throw new Error("Password is required");
+
+        const passwordMatch = await this.bcryptProvider.compare(
+            password,
+            user.password as string,
+        );
+
+        if (!passwordMatch) throw new Error("Email/password incorrect");
+
+        const token = sign(
+            {
+                id: user.id,
+            },
+            `${process.env.JWT_SECRET}`,
+            {
+                subject: `${user.id}`,
+                expiresIn: "1h",
+            },
+        );
+
+        delete user.password;
+        return {
+            token,
+            user,
+        };
+    }
+}
