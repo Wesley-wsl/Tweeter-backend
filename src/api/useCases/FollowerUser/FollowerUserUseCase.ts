3ae5e667a040b54ce0a28bf74b0a60c7f29c0382
Wesley Westelley <wesleywestelley@gmail.com>
2022-04-10 00:28:35 -0300

    :white_check_mark: feat: create useCase for follower user

---
+++ src/api/useCases/FollowerUser/FollowerUserUseCase.ts
@@ -0,0 +1,24 @@
+import { IUsersRepository } from "../../repositories/IUsersRepository";
+import { IFollowerUserDTO } from "./FollowerUserDTO";
+
+export class FollowerUserUseCase {
+    constructor(private usersRepository: IUsersRepository) {}
+
+    async execute({ id, userId }: IFollowerUserDTO): Promise<void> {
+        const userToFollow = await this.usersRepository.findById(id, true);
+        const user = await this.usersRepository.findById(userId, true);
+
+        if (!user) throw new Error("User not found");
+        if (!userToFollow) throw new Error("User to follow not found");
+        if (user.id === userToFollow.id)
+            throw new Error("User can't follow themselves");
+
+        delete user.password;
+        delete userToFollow.password;
+
+        user.following.push(userToFollow);
+        userToFollow.followers.push(user);
+        await this.usersRepository.save(user);
+        await this.usersRepository.save(userToFollow);
+    }
+}
