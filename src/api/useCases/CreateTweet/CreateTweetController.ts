0e5a543fdd01e817ffd4a246e449adc3643e1cf5
Wesley Westelley <wesleywestelley@gmail.com>
2022-04-11 01:09:46 -0300

    :wrench: feat: create controller for create tweet

---
+++ src/api/useCases/CreateTweet/CreateTweetController.ts
@@ -0,0 +1,23 @@
+import { Request, Response } from "express";
+
+import { CreateTweetUseCase } from "./CreateTweetUseCase";
+
+export class CreateTweetController {
+    constructor(private createTweetUseCase: CreateTweetUseCase) {}
+
+    async handle(request: Request, response: Response): Promise<Response> {
+        const { userId } = response.locals;
+        const { content, isPublic = "true" } = request.body;
+        const tweet = await this.createTweetUseCase.execute({
+            author_id: userId,
+            image: request.file?.filename ? request.file?.filename : undefined,
+            content,
+            isPublic,
+        });
+
+        return response.status(200).json({
+            success: true,
+            tweet,
+        });
+    }
+}
