2f916856adc8eb5f8cdc530de0c971a7f5aa48d4
Wesley Westelley <wesleywestelley@gmail.com>
2022-04-11 01:11:00 -0300

    :white_check_mark: feat: create tweet use case

---
+++ src/api/useCases/CreateTweet/CreateTweetUseCase.ts
@@ -0,0 +1,32 @@
+import { ITweetsRepository } from "../../repositories/ITweetsRepository";
+import { IUsersRepository } from "../../repositories/IUsersRepository";
+import { ICreateTweetDTO } from "./CreateTweetDTO";
+
+export class CreateTweetUseCase {
+    constructor(
+        private tweetsRepository: ITweetsRepository,
+        private usersRepository: IUsersRepository,
+    ) {}
+
+    async execute(tweet: ICreateTweetDTO) {
+        const authorFind = await this.usersRepository.findById(
+            tweet.author_id,
+            true,
+        );
+        if (!authorFind) throw new Error("Author not found");
+        const createTweet = await this.tweetsRepository.createTweet(tweet);
+
+        const parseBoolean = ["true", "false"].includes(tweet.isPublic)
+            ? true
+            : null;
+
+        if (parseBoolean === null)
+            throw new Error("isPublic should be true or false");
+
+        authorFind.tweets.push(createTweet);
+        this.usersRepository.save(authorFind);
+        this.tweetsRepository.save(createTweet);
+
+        return createTweet;
+    }
+}
