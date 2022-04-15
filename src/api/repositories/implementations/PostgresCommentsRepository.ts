cb1fc3b7f93ab46fc30974f75a96d5098818f5cb
Wesley Westelley <wesleywestelley@gmail.com>
2022-04-15 01:03:14 -0300

    :wrench: feat: create repository to comments

---
+++ src/api/repositories/implementations/PostgresCommentsRepository.ts
@@ -0,0 +1,39 @@
+import { Comment } from "../../entities/Comment";
+import { ICreateCommentDTO } from "../../useCases/CreateComment/CreateCommentDTO";
+import { ICommentRepository } from "../ICommentsRepository";
+
+export class PostgresCommentsRepository implements ICommentRepository {
+    public async findById(
+        id: string,
+        relation?: boolean,
+    ): Promise<Comment | null> {
+        return Comment.findOne({
+            where: { id },
+            relations: relation ? ["tweet"] : [],
+        });
+    }
+
+    public async createComment({
+        comment,
+        author_id,
+        tweet_id,
+        image,
+    }: ICreateCommentDTO): Promise<Comment> {
+        const commentCreated = Comment.create({
+            comment,
+            author_id,
+            tweet_id,
+            image,
+        });
+        await Comment.save(commentCreated);
+        return commentCreated;
+    }
+
+    public async save(comment: Comment): Promise<Comment> {
+        return Comment.save(comment);
+    }
+
+    public async delete(id: string): Promise<void> {
+        await Comment.delete(id);
+    }
+}
