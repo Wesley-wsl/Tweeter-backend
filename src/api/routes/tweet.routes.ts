64a981a168d80556c8bd3c38fe17b60b234b2f10
Wesley Westelley <wesleywestelley@gmail.com>
2022-04-15 00:06:23 -0300

    :wrench: feat: add all routes for tweet

---
+++ src/api/routes/tweet.routes.ts
@@ -0,0 +1,43 @@
+import { Router } from "express";
+import multer from "multer";
+
+import multerConfig from "../../config/multer";
+import { ensureAuthenticated } from "../middlewares/ensureAuthenticated";
+import { createTweetController } from "../useCases/CreateTweet";
+import { likeTweetController } from "../useCases/LikeTweet";
+import { saveTweetController } from "../useCases/SaveTweet";
+import { showTweetByUserController } from "../useCases/ShowTweetByUser";
+import { showTweetsController } from "../useCases/ShowTweets";
+import { unlikeTweetController } from "../useCases/UnlikeTweet";
+import { unsaveTweetController } from "../useCases/UnsaveTweet";
+
+const routes = Router();
+
+routes.post(
+    "/",
+    ensureAuthenticated,
+    multer(multerConfig).single("image"),
+    (request, response) => {
+        return createTweetController.handle(request, response);
+    },
+);
+routes.get("/", ensureAuthenticated, (request, response) => {
+    return showTweetsController.handle(request, response);
+});
+routes.get("/:authorId", ensureAuthenticated, (request, response) => {
+    return showTweetByUserController.handle(request, response);
+});
+routes.put("/:tweetId", ensureAuthenticated, (request, response) => {
+    return saveTweetController.handle(request, response);
+});
+routes.put("/:tweetId/like", ensureAuthenticated, (request, response) => {
+    return likeTweetController.handle(request, response);
+});
+routes.delete("/:tweetId/like", ensureAuthenticated, (request, response) => {
+    return unlikeTweetController.handle(request, response);
+});
+routes.delete("/:tweetId", ensureAuthenticated, (request, response) => {
+    return unsaveTweetController.handle(request, response);
+});
+
+export { routes as tweetRouter };
