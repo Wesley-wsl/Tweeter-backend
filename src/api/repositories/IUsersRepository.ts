d69517cd66f7b1ce07fbb30c551b090f24137c13
Wesley Westelley <wesleywestelley@gmail.com>
2022-04-15 01:02:16 -0300

    :pencil2: chore: add new method for interface

--- src/api/repositories/IUsersRepository.ts
+++ src/api/repositories/IUsersRepository.ts
@@ -4,6 +4,7 @@ import { ICreateUserDTO } from "../useCases/CreateUser/CreateUserDTO";
 export interface IUsersRepository {
     findByEmail(email: string, relation?: boolean): Promise<User | null>;
     findById(id: string, relation?: boolean): Promise<User | null>;
+    findAllUsers(): Promise<User[]>;
     createUser(user: ICreateUserDTO): Promise<User>;
     save(user: User): Promise<User>;
 }
