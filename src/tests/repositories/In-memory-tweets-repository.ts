083f10fedf63f9fd36596aeceefce7a0f9e70f28
Wesley Westelley <wesleywestelley@gmail.com>
2022-04-15 00:57:25 -0300

    :white_check_mark: feat: create in memory repository to tweets

---
+++ src/tests/repositories/In-memory-tweets-repository.ts
@@ -0,0 +1,36 @@
+import { Tweet } from "../../api/entities/Tweet";
+import { PostgresTweetsRepository } from "../../api/repositories/implementations/PostgresTweetsRepository";
+import { ICreateTweetDTO } from "../../api/useCases/CreateTweet/CreateTweetDTO";
+
+export class InMemoryTweetsRepository implements PostgresTweetsRepository {
+    public items: Tweet[] = [];
+
+    public async findAllTweets(): Promise<Tweet[]> {
+        return this.items;
+    }
+
+    async findById(id: string): Promise<Tweet | null> {
+        const tweetFind = this.items.find(tweet => tweet.id === id);
+        if (!tweetFind) return null;
+        return tweetFind;
+    }
+
+    async findByAuthor(author_id: string): Promise<Tweet[] | null> {
+        const tweetFind = this.items.filter(
+            tweet => tweet.author_id === author_id,
+        );
+        if (tweetFind.length === 0) return null;
+        return tweetFind;
+    }
+
+    async createTweet(tweet: ICreateTweetDTO): Promise<Tweet> {
+        const tweetCreated = tweet;
+        this.items.push(tweet as Tweet);
+        return tweetCreated as Tweet;
+    }
+
+    async save(tweet: Tweet): Promise<Tweet> {
+        this.items.push(tweet);
+        return tweet;
+    }
+}
