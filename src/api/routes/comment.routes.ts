45e05adb4db7da81c65533b53d146f9c6d77ce65
Wesley Westelley <wesleywestelley@gmail.com>
2022-04-16 16:08:28 -0300

    :white_check_mark: feat: create all routes for comments

---
+++ src/api/routes/comment.routes.ts
@@ -0,0 +1,31 @@
+import { Router } from "express";
+import multer from "multer";
+
+import multerConfig from "../../config/multer";
+import { ensureAuthenticated } from "../middlewares/ensureAuthenticated";
+import { createCommentController } from "../useCases/CreateComment";
+import { deleteCommentController } from "../useCases/DeleteComment";
+import { likeCommentController } from "../useCases/LikeComment";
+import { unlikeCommentController } from "../useCases/UnlikeComment";
+
+const routes = Router();
+
+routes.post(
+    "/:tweetId",
+    ensureAuthenticated,
+    multer(multerConfig).single("image"),
+    (request, response) => {
+        return createCommentController.handle(request, response);
+    },
+);
+routes.put("/:commentId/like", ensureAuthenticated, (request, response) => {
+    return likeCommentController.handle(request, response);
+});
+routes.delete("/:commentId/like", ensureAuthenticated, (request, response) => {
+    return unlikeCommentController.handle(request, response);
+});
+routes.delete("/:commentId", ensureAuthenticated, (request, response) => {
+    return deleteCommentController.handle(request, response);
+});
+
+export { routes as commentRouter };
