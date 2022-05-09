b20347d6faa5fbe8fe8e33396c039d3ef59726fa
Wesley Westelley <wesleywestelley@gmail.com>
2022-05-09 01:06:03 -0300

    :recycle: refactor: add path for migration and entities

--- src/api/database/index.ts
+++ src/api/database/index.ts
@@ -6,4 +6,15 @@ config();
 export const AppDataSource = new DataSource({
     type: "postgres",
     url: process.env.DATABASE_URI,
+    entities: [
+        `${__dirname}/../entities/*.ts`,
+        `${__dirname}/../entities/*.js`,
+    ],
+    migrations: [
+        `${__dirname}/migrations/*.ts`,
+        `${__dirname}/migrations/*.js`,
+    ],
+    synchronize: false,
+    logging: false,
+    migrationsRun: false,
 });
