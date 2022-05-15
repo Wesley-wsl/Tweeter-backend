33a8ec805e88a1c4efcf65292482ec2742e2769e
Wesley Westelley <wesleywestelley@gmail.com>
2022-05-15 16:09:28 -0300

    :test_tube: test: add pagination in find by author

--- src/tests/repositories/In-memory-tweets-repository.ts
+++ src/tests/repositories/In-memory-tweets-repository.ts
@@ -1,9 +1,25 @@
 import { Tweet } from "../../api/entities/Tweet";
+import { IPaginatedFetchResponse } from "../../api/interfaces/Paginated";
 import { PostgresTweetsRepository } from "../../api/repositories/implementations/PostgresTweetsRepository";
 import { ICreateTweetDTO } from "../../api/useCases/CreateTweet/CreateTweetDTO";
 
 export class InMemoryTweetsRepository implements PostgresTweetsRepository {
     public items: Tweet[] = [];
+
+    public async findByAuthorPaginated(
+        author_id: string,
+        page: number,
+        relation?: boolean,
+    ): Promise<IPaginatedFetchResponse<Tweet>> {
+        const pages = page * 10;
+
+        const response: IPaginatedFetchResponse<Tweet> = {
+            data: this.items.slice(pages, pages + 10),
+            total: this.items.length,
+        };
+
+        return response;
+    }
 
     public async findAllTweets(): Promise<Tweet[]> {
         return this.items;
