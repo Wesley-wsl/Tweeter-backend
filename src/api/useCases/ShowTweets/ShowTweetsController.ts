04432a388f306c7a0e918e71eefc3eb518f49637
Wesley Westelley <wesleywestelley@gmail.com>
2022-05-31 07:07:56 -0300

    :white_check_mark: feat: add pagination for show tweets

--- src/api/useCases/ShowTweets/ShowTweetsController.ts
+++ src/api/useCases/ShowTweets/ShowTweetsController.ts
@@ -7,12 +7,18 @@ export class ShowTweetsController {
 
     async handle(request: Request, response: Response): Promise<Response> {
         const { userId } = response.locals;
+        const { filter, page, search } = request.query;
 
-        const tweets = await this.showTweetsUseCase.execute(userId);
+        const tweets = await this.showTweetsUseCase.execute({
+            userId,
+            page: page !== undefined ? Number(page) : 0,
+            filter: filter !== undefined ? String(filter) : "",
+            search: search !== undefined ? String(search) : "",
+        });
 
         return response.status(200).json({
             success: true,
-            data: tweets,
+            ...tweets,
         });
     }
 }
--- src/api/useCases/ShowTweets/ShowTweetsUseCase.ts
+++ src/api/useCases/ShowTweets/ShowTweetsUseCase.ts
@@ -1,31 +1,54 @@
 /* eslint-disable no-param-reassign */
 import { Tweet } from "../../entities/Tweet";
+import { IPaginatedResponse } from "../../interfaces/Paginated";
 import { ITweetsRepository } from "../../repositories/ITweetsRepository";
+import { sortByLatest, sortByLikes } from "../../utils/tweets-utils";
+import { IShowTweetsDTO } from "./ShowTweetsDTO";
 
 export class ShowTweetsUseCase {
     constructor(private tweetsRepository: ITweetsRepository) {}
 
-    async execute(userId: string): Promise<Tweet[]> {
-        const tweets = await this.tweetsRepository.findAllTweets();
-        const tweetFiltered: Tweet[] = [];
+    async execute({
+        filter,
+        page,
+        userId,
+        search,
+    }: IShowTweetsDTO): Promise<IPaginatedResponse<Tweet>> {
+        let tweets = await this.tweetsRepository.findAllTweets(search);
 
-        tweets.forEach(tweet => {
-            if (tweet.isPublic === "false") {
-                if (!tweet.author.followers)
-                    throw new Error("Follower not found");
-                const isFollowing = tweet.author.followers.some(
-                    follower => follower.id === userId,
-                );
-                delete tweet.author.followers;
-                delete tweet.author.password;
-                if (isFollowing || tweet.author_id === userId)
-                    tweetFiltered.push(tweet);
-            }
-            delete tweet.author.followers;
+        if (!tweets) throw new Error("Tweets not fond");
+        if (filter === "media")
+            tweets = tweets.filter(tweet => tweet.image !== null);
+
+        tweets = tweets.filter(tweet => {
             delete tweet.author.password;
-            if (tweet.isPublic === "true") tweetFiltered.push(tweet);
+            const isFollowing = tweet.author.followers_id?.some(
+                follower => follower === userId,
+            );
+            return isFollowing || (tweet.author_id === userId && tweet);
         });
 
-        return tweetFiltered;
+        const total = tweets.length;
+        let totalPages = Math.ceil(total / 10);
+
+        if (totalPages - 1 < 1) totalPages = 1;
+        if (page > totalPages - 1) throw new Error("Page don't exists.");
+
+        if (filter === "top") sortByLikes(tweets);
+        if (filter === "latest") sortByLatest(tweets);
+
+        const tweetsPaginated = tweets.slice(page * 10, page * 10 + 10);
+        const response: IPaginatedResponse<Tweet> = {
+            data: tweetsPaginated,
+            totalElements: total,
+            page,
+            elements: tweetsPaginated.length,
+            elementsPerPage: 10,
+            totalPages,
+            firstPage: page === 0,
+            lastPage: page === totalPages - 1,
+        };
+
+        return response;
     }
 }
