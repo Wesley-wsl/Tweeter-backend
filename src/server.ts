9b948be84564a18beb46c3abae724ebd5b495f17
Wesley Westelley <wesleywestelley@gmail.com>
2022-07-16 01:44:15 -0300

    :white_check_mark: feat: configure documentation with swagger

--- src/server.ts
+++ src/server.ts
@@ -5,11 +5,13 @@ import cors from "cors";
 import { config } from "dotenv";
 import express from "express";
 import { resolve } from "path";
+import swaggerUi from "swagger-ui-express";
 
 import { AppDataSource } from "./api/database";
 import { errorHandler } from "./api/middlewares/errorHandler";
 import { tweetRouter, userRouter } from "./api/routes";
 import { commentRouter } from "./api/routes/comment.routes";
+import swaggerJSON from "./config/swagger/index.json";
 
 config();
 AppDataSource.initialize();
@@ -18,6 +20,7 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerJSON));
 app.use("/files", express.static(resolve(__dirname, "..", "uploads")));
 app.use("/api/v1/user", userRouter);
 app.use("/api/v1/tweet", tweetRouter);
