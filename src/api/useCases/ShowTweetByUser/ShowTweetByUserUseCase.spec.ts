e5ad5a852c96ac0e345a7e460095901f1c0c4638
Wesley Westelley <wesleywestelley@gmail.com>
2022-07-17 00:10:37 -0300

    :test_tube: test: ensure that cases of filter work correctly

--- src/api/useCases/ShowTweetByUser/ShowTweetByUserUseCase.spec.ts
+++ src/api/useCases/ShowTweetByUser/ShowTweetByUserUseCase.spec.ts
@@ -32,12 +32,13 @@ describe("#ShowTweetByUser", () => {
         const response = await sut.execute({
             userId: user.id,
             authorId: tweet.author_id,
+            filter: "",
             page: 0,
         });
         expect(response.data.length).toEqual(0);
     });
 
-    it("Shouldn't show tweets if user is not found", async () => {
+    it("Shouldn't show tweets if tweets by author is not found", async () => {
         const inMemoryUsersRepository = new InMemoryUsersRepository();
         const inMemoryTweetsRepository = new InMemoryTweetsRepository();
         const sut = new ShowTweetByUserUseCase(
@@ -45,21 +46,14 @@ describe("#ShowTweetByUser", () => {
             inMemoryUsersRepository,
         );
 
-        const tweet = {
-            id: "123423243423",
-            author_id: "1",
-            isPublic: "false",
-            author: {},
-        };
-
-        inMemoryTweetsRepository.items.push(tweet as Tweet);
-
-        const response = await sut.execute({
+        const response = sut.execute({
             userId: "2",
-            authorId: tweet.author_id,
+            authorId: "20",
+            filter: "",
             page: 0,
         });
-        expect(response.data.length).toEqual(0);
+
+        await expect(response).rejects.toEqual(new Error("Tweets not fond"));
     });
 
     it("Should list tweets private by user if is following he", async () => {
@@ -96,8 +90,164 @@ describe("#ShowTweetByUser", () => {
         const response = await sut.execute({
             userId: user.id,
             authorId: tweet.author_id,
+            filter: "",
             page: 0,
         });
         expect(response.data[0].isPublic).toEqual("false");
+    });
+
+    it("Should show tweets filtered by media.", async () => {
+        const inMemoryUsersRepository = new InMemoryUsersRepository();
+        const inMemoryTweetsRepository = new InMemoryTweetsRepository();
+        const sut = new ShowTweetByUserUseCase(
+            inMemoryTweetsRepository,
+            inMemoryUsersRepository,
+        );
+
+        const user = {
+            id: "1",
+            following: [],
+            following_id: [],
+        };
+
+        inMemoryUsersRepository.items.push(user as unknown as IUser);
+
+        const tweet = {
+            id: "123423243423",
+            author_id: user.id,
+            isPublic: "false",
+            author: {},
+            image: null,
+            likes: 10,
+        };
+
+        const tweetTwo = {
+            id: "123",
+            author_id: user.id,
+            isPublic: "false",
+            author: {},
+            image: "/image.png",
+            likes: 100,
+        };
+
+        inMemoryTweetsRepository.items.push(tweet as unknown as Tweet);
+        inMemoryTweetsRepository.items.push(tweetTwo as Tweet);
+
+        const response = await sut.execute({
+            userId: user.id,
+            authorId: tweet.author_id,
+            filter: "media",
+            page: 0,
+        });
+
+        expect(response.data[0].image).toBeTruthy();
+        expect(response.data[1]).toBeFalsy();
+    });
+
+    it("Should be able to show tweets filtered by likes", async () => {
+        const inMemoryUsersRepository = new InMemoryUsersRepository();
+        const inMemoryTweetsRepository = new InMemoryTweetsRepository();
+        const sut = new ShowTweetByUserUseCase(
+            inMemoryTweetsRepository,
+            inMemoryUsersRepository,
+        );
+
+        const user = {
+            id: "1",
+            following: [],
+            following_id: [],
+        };
+
+        inMemoryUsersRepository.items.push(user as unknown as IUser);
+
+        const tweet = {
+            id: "123423243423",
+            author_id: user.id,
+            isPublic: "false",
+            author: {},
+            likes: 10,
+        };
+
+        const tweetTwo = {
+            id: "123423243423",
+            author_id: user.id,
+            isPublic: "false",
+            author: {},
+            likes: 100,
+        };
+
+        inMemoryTweetsRepository.items.push(tweet as Tweet);
+        inMemoryTweetsRepository.items.push(tweetTwo as Tweet);
+
+        const response = await sut.execute({
+            userId: user.id,
+            authorId: tweet.author_id,
+            filter: "likes",
+            page: 0,
+        });
+
+        expect(response.data[0].likes >= response.data[1].likes).toEqual(true);
+    });
+
+    it("Shoulnd't be able to show tweets if page don't exists.", async () => {
+        const inMemoryUsersRepository = new InMemoryUsersRepository();
+        const inMemoryTweetsRepository = new InMemoryTweetsRepository();
+        const sut = new ShowTweetByUserUseCase(
+            inMemoryTweetsRepository,
+            inMemoryUsersRepository,
+        );
+
+        const user = {
+            id: "1",
+            following: [],
+            following_id: [],
+        };
+
+        inMemoryUsersRepository.items.push(user as unknown as IUser);
+
+        const tweet = {
+            id: "123423243423",
+            author_id: user.id,
+            isPublic: "false",
+            author: {},
+        };
+
+        inMemoryTweetsRepository.items.push(tweet as Tweet);
+
+        const response = sut.execute({
+            userId: user.id,
+            authorId: tweet.author_id,
+            filter: "",
+            page: 1,
+        });
+
+        await expect(response).rejects.toEqual(new Error("Page don't exists."));
+    });
+
+    it("Shouldn't be able to show tweets if user don't exists.", async () => {
+        const inMemoryUsersRepository = new InMemoryUsersRepository();
+        const inMemoryTweetsRepository = new InMemoryTweetsRepository();
+        const sut = new ShowTweetByUserUseCase(
+            inMemoryTweetsRepository,
+            inMemoryUsersRepository,
+        );
+
+        const tweet = {
+            id: "123423243423",
+            author_id: "200",
+            isPublic: "false",
+            author: {},
+        };
+
+        inMemoryTweetsRepository.items.push(tweet as Tweet);
+
+        const response = await sut.execute({
+            userId: "2",
+            authorId: tweet.author_id,
+            filter: "",
+            page: 0,
+        });
+
+        expect(response.data.length === 0).toEqual(true);
     });
 });
