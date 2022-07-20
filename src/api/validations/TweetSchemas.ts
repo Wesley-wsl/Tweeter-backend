00c97bc3cc3651a669045689397a061e9f685ad9
Wesley Westelley <wesleywestelley@gmail.com>
2022-07-19 21:16:32 -0300

    :wrench: chore: increase maximum content

--- src/api/validations/TweetSchemas.ts
+++ src/api/validations/TweetSchemas.ts
@@ -1,7 +1,7 @@
 import { Joi } from "celebrate";
 
 export const createTweetSchema = Joi.object().keys({
-    content: Joi.string().max(500).min(50).required(),
+    content: Joi.string().max(800).min(20).required(),
     isPublic: Joi.string().valid("true", "false").optional(),
     tweet_id: Joi.string().optional(),
 });
